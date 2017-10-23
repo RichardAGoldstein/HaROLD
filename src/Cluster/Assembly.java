32ddb7a4ec37d0eaddb9dcbb8fec18233bea4ccd
richfrommich <r.goldstein@ucl.ac.uk>
2017-10-23 10:23:21 +0100

    Good progress over weekend

--- src/Cluster/Assembly.java
+++ src/Cluster/Assembly.java
@@ -25,6 +25,7 @@ public class Assembly {
     int[][] strandReads = new int[2][4];  // strand and base
     int[] reads = new int[4];  // sum for each base
     int totReads = 0;
+    int[] totStrand = new int[2];
     double[] minAmt = new double[4];
     
     boolean hasData = true;
@@ -35,37 +36,69 @@ public class Assembly {
         String[] words = inputLine.split(",");
         iSite = Integer.parseInt(words[0]);
 
-        int[] totStrand = new int[2];
         for (int iBase = 0; iBase < 4; iBase++) {
             for (int iStrand = 0; iStrand < 2; iStrand++) {
                 strandReads[iStrand][iBase]=Integer.parseInt(words[(2*iBase)+iStrand+3]);
                 totStrand[iStrand] += strandReads[iStrand][iBase];
+                reads[iBase] += strandReads[iStrand][iBase];
+                totReads += strandReads[iStrand][iBase];
             }
-            reads[iBase] = strandReads[0][iBase]+strandReads[1][iBase];
-            totReads += reads[iBase];
         }
         if (totReads == 0) {
             hasData = false;
 
         } else if (Math.max(Math.max(reads[0],reads[1]),Math.max(reads[2],reads[3])) == totReads) {
             conserved = true;
-        } // estimate lower bound on number of reads of each base
-        double bias = Math.abs(totStrand[0]-totStrand[1]) / (Math.sqrt(totStrand[0]) + Math.sqrt(totStrand[1]));
-        if (totReads > 99) {
-            System.out.print(iSite + "\t" + bias);
-            BinomialDistribution bd = new BinomialDistribution(totReads, 0.5);
-            int n1 = bd.sample();
-            int n2 = totReads - n1;
-            bias = Math.abs(n1-n2) / (Math.sqrt(n1) + Math.sqrt(n2));
-            System.out.println("\t" + bias + "\t" + totStrand[0] + "\t" + totStrand[1] );
-        }
-        for (int iBase = 0; iBase < 4; iBase++) {
-            if (reads[iBase] > 0) {   // downweight for excessive strand bias
-                double ratio = (strandReads[0][iBase]+1.0)/(reads[iBase]+2.0);
-                double zScore = Math.abs(ratio-0.5)*Math.sqrt(2.0*reads[iBase]);
-                double pVal = (1.0 - Erf.erf(zScore));
-                minAmt[iBase] = (pVal/(0.01 + pVal))  // consider lowest CI value
-                    * inverseBeta(0.025, (0.5+reads[iBase]), (0.5+totReads-reads[iBase]));
+        } else {
+            double topRatio = 0.00000;
+            int iConsensus = -999;
+            boolean printThis = false;
+            for (int iBase = 0; iBase < 4; iBase++) {
+                if (reads[iBase] > 0) {
+                    int[] n = new int[2];
+                    int[] m = new int[2];
+                    double[] ratio = new double[2];
+                    n[0] = strandReads[0][iBase];
+                    n[1] = strandReads[1][iBase];
+                    for (int jBase = 0; jBase < 4; jBase++) {
+                        if (iBase != jBase) {
+                            m[0] += strandReads[0][jBase];
+                            m[1] += strandReads[1][jBase];
+                        }
+                    }
+                    ratio[0] = 0.0;
+                    ratio[1] = 0.0;
+                    if (totStrand[0] > 0) {
+                        ratio[0] = (1.0 * n[0]) / (1.0 * (n[0] + m[0]));
+                    }
+                    if (totStrand[1] > 0) {
+                        ratio[1] = (1.0 * n[1]) / (1.0 * (n[1] + m[1]));
+                    }
+//                    System.out.println(n[0] + "\t" + m[0] + "\t" + n[1] + "\t" + m[1] + "\t" + Arrays.toString(ratio));
+                    if (Math.max(ratio[0], ratio[1]) > topRatio) {
+                        topRatio = Math.max(ratio[0], ratio[1]);
+                        iConsensus = iBase;
+                    }
+                    double independent = - Math.log(n[0] + m[0] +1.0) - Math.log(n[1] + m[1] + 1.0);
+                    double correlatedNum = CombinatoricsUtils.factorialLog(n[0]+m[0]) 
+                            + CombinatoricsUtils.factorialLog(n[1]+m[1]) 
+                            + CombinatoricsUtils.factorialLog(n[0]+n[1]) 
+                            + CombinatoricsUtils.factorialLog(m[0]+m[1]);
+                    double correlatedDen = CombinatoricsUtils.factorialLog(n[0]) 
+                            + CombinatoricsUtils.factorialLog(n[1]) 
+                            + CombinatoricsUtils.factorialLog(m[0]) 
+                            + CombinatoricsUtils.factorialLog(m[1])
+                            + CombinatoricsUtils.factorialLog(n[0] + n[1] + m[0] + m[1] + 1);
+                    double correlated = correlatedNum - correlatedDen;
+                    double posterior = 0.99 * Math.exp(correlated) / (0.99 * Math.exp(correlated) + 0.01 * Math.exp(independent));
+                    minAmt[iBase] = posterior * inverseBeta(0.025, (0.5+reads[iBase]), (0.5+totReads-reads[iBase]));
+                    if (posterior < 0.01) printThis = true;
+                }
+            }
+            minAmt[iConsensus] = 1.0;
+            if (printThis) {
+                System.out.println(Arrays.toString(strandReads[0]) + "\t" + Arrays.toString(strandReads[1])
+                + "\t" + Arrays.toString(minAmt));
             }
         }
     }
